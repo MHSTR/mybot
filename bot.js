1733d481fd243e6f56997f6788d1ee3fe154fe14
MHSTR <38856850+MHSTR@users.noreply.github.com>
2018-10-03 11:11:22 -0930

    Update bot.js

--- bot.js
+++ bot.js
@@ -906,11 +906,11 @@ await message.channel.send("supports test")
               }, 3000)
               rokzz.send(`
                 تقديم من ${rokz.author}
-  اللغه:
-  \`${lang}\`
-  المده:
+الجواب 1
+\`${lang}\`
+  الجواب 2:
   \`${time}\`
-  الخبره:
+  الجواب 3:
   \`${expe}\`
                 `).then(rokzzz => {
                   rokzzz.react("✅")
@@ -1020,37 +1020,6 @@ client.on('message' , message => {
 }
  });
     
-client.on('message', message => {
-let perm = message.guild.member(message.author).hasPermission('ADMINISTRATOR') || message.guild.member(message.author).hasPermission('BAN_MEMBERS')
-if (!perm) return message.reply(':x: | **You don\'t have `BAN_MEMBERS` permission to use this command**.')
-if(message.content.startsWith(prefix + '--ban')) {
-  let nourid = message.content.split(" ").slice(3).join(" ");
-  client.fetchUser(nourid).then(id => {
-    message.guild.ban(id).catch(err => {
-      message.channel.send("Error 404, failed to ban this user :( -> " +id)
-      console.log(err)
-    })
-    message.channel.send(`I banned the user ${id} successfully.`)
-  }).catch(() => {
-    message.channel.send(`Theres no user with the ID of ${nourid}, please try again. :face_palm:`)
-  })
-  }});
-client.on('message', message => {
-let perm = message.guild.member(message.author).hasPermission('ADMINISTRATOR') || message.guild.member(message.author).hasPermission('BAN_MEMBERS')
-if (!perm) return message.reply(':x: | **You don\'t have `BAN_MEMBERS` permission to use this command**.')
-if(message.content.startsWith(prefix + '-unban')) {
-  let nourid = message.content.split(" ").slice(3).join(" ");
-  let nour = bot.fetchUser(nourid)
-  .then(user => {
-    message.guild.unban(user.id)
-    .then(() => {
-      message.channel.send(`Alright, I unhackbanned ${user}.`)
-    }).catch(err => {
-        message.channel.send(`Failed to unban :( ${user}`)
-    })
-  }).catch(() => message.channel.send("Theres no user with the this ID :face_palm:"))
-}
-  })
    client.on('message', message => {
     if(message.author.bot) return; // Alpha Codes Server.
     if(message.channel.type === 'dm') return;
